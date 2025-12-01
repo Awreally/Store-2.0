41446a617ed417b6484102412df135eaf322fe96
Pontus Ingenius <Pontusingenius@gmail.com>
2025-12-01 15:37:00 +0100

    productCard done

--- JS/productCard.js
+++ JS/productCard.js
@@ -1,19 +1,30 @@
 export class Card {
-    constructor({ title, description, image, price, primaryAction}) {
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.body = body;
-        this.image = images[0];
-        this.primaryAction = primaryAction;
-    }
-    createCard() {
-        const articleCard = document.createElement('article');
-        articleCard.className = 'card';
+  constructor({ title, description, image, price, primaryAction }) {
+    this.title = title;
+    this.description = description;
+    this.price = price;
+    this.image = image;
+    this.primaryAction = primaryAction;
+  }
+  createCard() {
+    const articleCard = document.createElement("article");
+    articleCard.className = "card";
+    
+    const imgCard = document.createElement("img");
+    imgCard.className = "imgcard";
+    imgCard.src = this.image;
+    articleCard.appendChild(imgCard);
 
-        articleCard.innerHTML = `
-            <img class="cardImg" ${this.image}>
-        `;
-    }
+    const titleCard = document.createElement("h2");
+    titleCard.className = "titlecard";
+    titleCard.textContent = this.title;
+    articleCard.appendChild(titleCard);
 
+    const priceCard = document.createElement("p");
+    priceCard.className = "pricecard";
+    priceCard.textContent = `Price: ${this.price} $`;
+    articleCard.appendChild(priceCard);
+
+    return articleCard;
+  }
 }
