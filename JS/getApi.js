d8112e4aef9cc330307c94031332afc1ced25005
Pontus Ingenius <Pontusingenius@gmail.com>
2025-12-02 12:14:48 +0100

    Added new API, products showing

--- JS/getApi.js
+++ JS/getApi.js
@@ -1,11 +1,11 @@
 export class GetApiData {
-    constructor(url = 'https://api.escuelajs.co/api/v1/products') {
+    constructor(url = 'https://fakestoreapi.com/products') {
         this.url = url;
         this.products = [];
     }
     async getData() {
     try {
-        const res = await fetch(this.urlrl);
+        const res = await fetch(this.url);
         if (!res.ok) throw new Error('Kunde inte hämta produkter');
         this.products = await res.json();
         return this.products;
--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 import {menuHover as menu} from "./JS/menuHover.js";
 import { GetApiData } from "./JS/getApi.js";
+import { uiComponents } from "./JS/uiComponents.js";
 
-const menuNav = document.querySelector('.navmenu');
+async function init() {
+    try {
+        const apiData = new GetApiData();
+        const data = await apiData.getData();
+        const ui = new uiComponents(".container");
+        ui.products = data;
+        ui.renderProduct();
+    } catch (error) {
+        showError('Loading error');
+    }
+}
+
+init();
 
