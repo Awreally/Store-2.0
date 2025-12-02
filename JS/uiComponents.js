63652035e84cd1c6f547508090afa6ff27c6acfd
Pontus Ingenius <Pontusingenius@gmail.com>
2025-12-02 13:08:01 +0100

    added Event listerner and data-xxx

--- JS/uiComponents.js
+++ JS/uiComponents.js
@@ -1,17 +1,17 @@
 import { Card } from "./productCard.js";
 
 export class uiComponents {
-    constructor(containerSelector) {
-        this.container = document.querySelector(containerSelector);
-        this.filteredProducts = [];
-        this.products = [];  
-    }
+  constructor(containerSelector) {
+    this.container = document.querySelector(containerSelector);
+    this.filteredProducts = [];
+    this.products = [];
+  }
 
-    renderProduct() {
-        this.products.forEach(product => {
-             const card = new Card(product)
-            const cardElement = card.createCard();
-            this.container.appendChild(cardElement);
-        });
-    }
+  renderProduct() {
+    this.products.forEach((product) => {
+      const card = new Card(product);
+      const cardElement = card.createCard();
+      this.container.appendChild(cardElement);
+    });
+  }
 }
--- main.js
+++ main.js
@@ -1,18 +1,38 @@
-import {menuHover as menu} from "./JS/menuHover.js";
+import { menuHover as menu } from "./JS/menuHover.js";
 import { GetApiData } from "./JS/getApi.js";
 import { uiComponents } from "./JS/uiComponents.js";
 
 async function init() {
-    try {
-        const apiData = new GetApiData();
-        const data = await apiData.getData();
-        const ui = new uiComponents(".container");
-        ui.products = data;
+  try {
+    const apiData = new GetApiData();
+    const data = await apiData.getData();
+    const allProducts = data;
+
+    const ui = new uiComponents(".container");
+    ui.products = allProducts;
+
+    const navMenu = document.querySelector(".navmenu");
+
+    navMenu.addEventListener("click", (e) => {
+      if (e.target.tagName !== "A") {
+        return;
+      }
+      const category = e.target.dataset.category;
+      if (!category) {
+        return;
+      }
+
+      ui.container.innerHTML = "";
+
+      if (category === "new") {
+        ui.products = allProducts;
         ui.renderProduct();
-    } catch (error) {
-        showError('Loading error');
-    }
+        return;
+      }
+    });
+  } catch (error) {
+    showError("Loading error");
+  }
 }
 
 init();
-
